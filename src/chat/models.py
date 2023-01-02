fe169896e34a1f687d514d5e989599eaf4e87906
Hylke Nicola <hyskon@outlook.com>
2023-01-02 21:51:25 +0100

    basic layout

--- src/chat/models.py
+++ src/chat/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+
+
+# maak een model voor de chatlog waardoor de questions en answers kunnen worden opgeslagen en gebruikt voor de chatbot als reference
