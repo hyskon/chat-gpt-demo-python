affa2d0f7fd7969d6a8ee05569a95515acaf32d0
Hylke Nicola <hyskon@outlook.com>
2023-01-03 18:32:06 +0100

    backern+frontend working+db-connection

--- src/chat/models.py
+++ src/chat/models.py
@@ -1,6 +1,22 @@
 from django.db import models
+from uuid import uuid4
+
 
 # Create your models here.
+def generateUUID():
+    return str(uuid4())
 
+class Conversation(models.Model):
+    conversation_id = models.CharField(max_length=255, unique=True, default=generateUUID, editable=False)
+    def __str__(self):
+        return "{}:{}..".format(self.id, self.conversation_id[:10])
 
-# maak een model voor de chatlog waardoor de questions en answers kunnen worden opgeslagen en gebruikt voor de chatbot als reference
+class Message(models.Model):
+    # Fields for messages
+    conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE)
+    message_id = models.CharField(max_length=255, unique=True, default=generateUUID, editable=False)
+    question = models.TextField()
+    answer = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return "{}:{}..".format(self.id, self.message_id[:10])
--- src/chat/views.py
+++ src/chat/views.py
@@ -1,3 +1,46 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import openai
+from .models import Conversation, Message
 
 # Create your views here.
+def home(request):
+    # Initialize chat input and chat response
+    chat_input = ''
+    chat_response = ''
+
+    # Check if there is chat input
+    if 'chat_input' in request.POST:
+        # Get chat input from user
+        chat_input = request.POST.get('chat_input')
+
+        # Create new conversation
+        conversation = Conversation.objects.create()
+
+        # Use GPT-2 model to generate a response
+        openai.api_key = "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"
+        response = openai.Completion.create(
+            model="text-davinci-002",
+            prompt=f"User: {chat_input}\nBot: ",
+            temperature=0.7,
+            max_tokens=1024,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0
+        )
+
+        # Get response text
+        chat_response = response['choices'][0]['text']
+
+        # Create new message
+        Message.objects.create(
+            conversation=conversation,
+            question=chat_input,
+            answer=chat_response,
+        )
+
+    # Render chat template with chat input and chat response
+    return render(request, 'chat/index.html', {
+        'chat_input': chat_input,
+        'chat_response': chat_response,
+    })
